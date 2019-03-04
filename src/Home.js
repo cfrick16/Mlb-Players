a116e1028cd7f2052519e6db101490c8bc8ec7f0
cfrick16 <cfrick16@gmail.com>
2019-03-04 03:17:01 -0600

    Added Technical Report

--- src/Home.js
+++ src/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
   handleSearchButton = () => {
     this.setState ({
       players: [],
-      flag: true,
+      flag: false,
       page: 0,
       search: this.refs.query.value
   });
@@ -61,6 +61,9 @@ class Home extends Component {
 
 
   render() {
+    if(!this.state.flag){
+      return(<></>);
+    }
     return(
     <>
 
